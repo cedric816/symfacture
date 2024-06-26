09010139a25c95534ec8949a9720fffeb1ad46c9
Cedric <cedricnoisette9@gmail.com>
2024-06-26 12:31:20 +0200

    create entity User\User

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home_index')]
+    #[Route('/', name: 'home_index', methods:['GET'])]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
